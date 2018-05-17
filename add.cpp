d036b45121d6706ed0a9abc8b489581ac0774ffc
Shikhar Chauhan <shikhar.chauhan@live.com>
2018-05-17 06:55:32 +0530

    Vector Addition using CUDA

---
+++ add.cpp
@@ -0,0 +1,37 @@
+/*
+ * Adding two vectors on CPU
+ */
+#include<iostream>
+#include<math.h>
+
+using namespace::std;
+
+void add(int n, float *x, float *y){
+  for(int i = 0; i < n; i++){
+    y[i] = x[i] + y[i];
+  }
+}
+
+int main(void){
+    int N = 1<<20;
+
+    float *x = new float[N];
+    float *y = new float[N];
+
+    for(int i=0; i<N; i++){
+        x[i] = 1.0f;
+        y[i] = 2.0f;
+    }
+
+    add(N,x,y);
+
+    float maxError = 0.0f;
+
+    for(int i=9; i < N; i++){
+      maxError = fmax(maxError, fabs(y[i] - 3.0f));
+    }
+    std::cout<<"Max Error:" << maxError << std::endl;
+    delete [] x;
+    delete []y;
+    return 0;
+}
